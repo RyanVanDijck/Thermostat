2357b6ea110c8d7929ee959bcd249c3e5dd8a45a
Ryan Van Dijck <rvandijck02@outlook.com>
2021-07-12 16:45:27 +0100

    Writing add test

--- spec/thermostat.spec.js
+++ spec/thermostat.spec.js
@@ -2,11 +2,17 @@
 
 describe('temperature tests', function(){
     it('initial test', function(){
-
         //SETUP
         const thermostat = new Thermostat();
-
+        
         //EXECUTE and VERIFY
         expect(thermostat.temperature).toEqual(20);
-   });
+    });
+    it('add function', () => {
+        //SETUP
+        const thermostat = new Thermostat(); 
+
+        //EXECUTE and VERIFY
+        expect(thermostat.add(2)).toEqual(22); 
+    }); 
 })
