91e015587856359054f554596bcf5611667be79b
Rawdah Chowdhury <rawdah98@outlook.com>
2021-07-12 17:40:12 +0100

    passed test 5

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,13 +1,23 @@
  class Thermostat{
     constructor(){
      this.temperature = 20;
+     this.powerSaving = true;
+
     }
     up(number){
-        return this.temperature += number; 
+        if(number + this.temperature <= this.getMaximumTemperature()){
+            return number + this.temperature
+        }
+        else{
+            return 'Maximum temperature is ' +this.getMaximumTemperature()
+        }
     }
     down(number){
         return this.temperature - number >= 10 ? this.temperature -= number
         : 'Minimum temperature is 10'; 
+    }
+    getMaximumTemperature(){
+        return this.powerSaving ? 25: 32
     }
 
 }
